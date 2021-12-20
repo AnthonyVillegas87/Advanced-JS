d0d98469405f5e090ce3f202d00130c69f6c8017
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-20 13:25:10 -0600

    feat: console object refresh

---
+++ js/refresh.js
@@ -0,0 +1,38 @@
+"use strict"
+
+document.write('Hello!');
+document.querySelector('h1').style.color = 'red';
+
+//console
+// The console object provides access to the browser's debugging console (e.g. the Web console in Firefox).
+// The specifics of how it works varies from browser to browser, but there is a de facto set of features that are typically provided.
+// Log to Console
+
+/*String*/console.log("Hello World!")
+/*Number*/console.log(123);
+/*Boolean*/console.log(false)
+/*Array*/console.log([1,2,3])
+/*Object Literal*/console.log({a:1, b:2})
+
+// Console.table
+console.table({a:1, b:2})
+
+//Console.error
+console.error('This is an error!')
+
+//Console.clear
+console.clear();
+
+//Console.warn
+console.warn('This is a warning!!');
+
+//Console.time
+console.time('Hello...')
+
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+
+console.time('Hey!')
+
