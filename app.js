28dc073374b091acf8c4f53bd19636b16ae9b579
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-27 14:11:18 -0600

    feat: event listeners & the event object

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ li.appendChild(link);
 document.querySelector('ul.collection').appendChild(li);
 
 console.log(li)
-*/
+
 
 //Replace Elements
 
@@ -222,3 +222,31 @@ val = link.hasAttribute('title');
 link.removeAttribute('title');
 val = link;
 console.log(val)
+*/
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+function onClick(e){
+    let val;
+    val = e;
+
+    //Event target element
+    val = e.target;
+    val = e.target.id;
+    val = e.target.className;
+    val = e.target.classList;
+
+    //Event type
+    val = e.type;
+
+    //Timestamp
+    val = e.timeStamp;
+    //Coords event relative to the window
+    val = e.clientY;
+    val = e.clientX;
+    //Coords event relative to the element
+    val = e.offsetY;
+    val = e.offsetX;
+
+    console.log(val)
+}
