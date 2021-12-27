dbaec4700071474d51e885162cf66b48dc7f2bf0
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-27 12:51:31 -0600

    feat: DOM selectors for single elements practice

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict"
+/*
 let val;
 
 val = document;
@@ -31,4 +32,38 @@ let scriptsArr = Array.from(scripts);
 scriptsArr.forEach(function(script) {
     console.log(script.getAttribute('src'))
 });
-console.log(val)
+console.log(val)*/
+
+// document.getElementById()
+console.log(document.getElementById('task-title'));
+
+//Change styling
+document.getElementById('task-title').style.background = '#344';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '5px';
+
+//Change content
+document.getElementById('task-title').textContent = 'Task List';
+document.getElementById('task-title').innerText = 'My Tasks';
+document.getElementById('task-title').innerHTML = '<span style="color: red">Task List</span>'
+
+//document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').style.color = 'green';
+
+
+
+
+
+
+
+
+
