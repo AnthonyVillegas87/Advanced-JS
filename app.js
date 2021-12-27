485adc1a909b8d9163beacd91134db15d1a6ff3b
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-27 13:12:14 -0600

    feat: DOM selectors for multiple elements

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ let scriptsArr = Array.from(scripts);
 scriptsArr.forEach(function(script) {
     console.log(script.getAttribute('src'))
 });
-console.log(val)*/
+console.log(val)
 
 // document.getElementById()
 console.log(document.getElementById('task-title'));
@@ -59,9 +59,55 @@ document.querySelector('li:last-child').style.color = 'red';
 document.querySelector('li:nth-child(3)').style.color = 'yellow';
 document.querySelector('li:nth-child(4)').style.color = 'green';
 
+//document.getElementsByClassName()
+
+const items = document.getElementsByClassName('collection-item');
+console.log(items)
+
+items[0].textContent = 'Hello';
+items[1].textContent = 'World';
+
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+console.log(listItems)
+
+//document.getElementsByTagName()
+let lis = document.getElementsByTagName('li');
+console.log(lis)
+
+//convert HTML collection into array
+lis = Array.from(lis)
+lis.reverse();
+
+
+lis.forEach(function(li) {
+    console.log(li.className);
+    li.textContent = 'Grocery Item';
+})
+
+console.log(lis)
+*/
+
+//document.querySelectorAll()
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+items.forEach(function(item, index) {
+    item.textContent = `${index}: Hello`
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(li, index) {
+    li.style.background = '#ccc';
+});
+
+for(let i = 0; i < liEven.length; i++) {
+    liEven[i].style.background = '#f4f4f4';
+}
 
 
 
+console.log(items);
 
 
 
