5f970f8f5321d1fb9217b60a5327009e8b327e55
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-27 13:41:39 -0600

    feat: creating elements

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ for(let i = 0; i < liEven.length; i++) {
 }
 
 console.log(items);
-*/
+
 
 let val;
 
@@ -147,6 +147,32 @@ val = listItem.nextElementSibling.nextElementSibling;
 val = listItem.previousSibling;
 val = listItem.previousElementSibling;
 console.log(val)
+*/
 
+//Creating elements
+const li = document.createElement('li');
+ //Add class
+li.className = 'collection-item';
+//Add id
+li.id = 'new-item';
+//Add attribute
+li.setAttribute('title', 'New Item');
+
+//Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+//Create new link element
+const link = document.createElement('a');
+//Add classes
+link.className = 'delete-item secondary-content';
+//Add icon HTML
+link.innerHTML = '<i class="fa fa-remove"></i>';
+//Append link to li
+li.appendChild(link);
+
+//Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li)
 
 
