73a7ad333f43dcbe51846578cb4b9df2a3f11055
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-11 12:24:33 -0600

    feat: created data for POST request & successful post with id created

--- js/http.js
+++ js/http.js
@@ -2,12 +2,26 @@
 const http = new EasyHTTP();
 
 //GET posts
- http.get('https://jsonplaceholder.typicode.com/posts' ,
-     function(error, response) {
-     if(error) {
+//  http.get('https://jsonplaceholder.typicode.com/posts' ,
+//      function(error, response) {
+//      if(error) {
+//          console.log(error);
+//      } else {
+//          console.log(response);
+//      }
+//  });
+
+//Create data
+const data = {
+    title: 'Custom Post',
+    body: 'This is a new post'
+};
+
+//create new post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post) {
+    if(error) {
          console.log(error);
      } else {
-         console.log(response);
+         console.log(post);
      }
-         console.log(response);
-     });
+});
