1341516c7ed5966c1cd5cb2ed2bd2296d011ebf3
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-14 14:37:25 -0600

    feat: completed async await function using fetch

--- js/async-await.js
+++ js/async-await.js
@@ -22,3 +22,16 @@
 // .then(response => document.write(response))
 //     .catch(error => console.log(error));
 
+async function getUsers() {
+    //await response of the fetch call
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    //only proceed once its resolved
+    const data = await response.json();
+
+    //only proceed once second promise is resolved
+    return data;
+}
+
+getUsers()
+    .then(users => console.log(users));
