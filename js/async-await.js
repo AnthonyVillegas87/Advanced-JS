194deca1d31f702cc9f9df24839ac393ba5ed1aa
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-14 14:32:45 -0600

    feat: async/await using fetch

--- js/async-await.js
+++ js/async-await.js
@@ -1,1 +1,24 @@
 "use strict"
+
+// async function myFunc() {
+//
+//
+//     const promise = new Promise((resolve, reject) => {
+// setTimeout(() => resolve('Hello!!!!!!!!'), 1000);
+//     })
+//
+//     const error = false;
+//     if(!error) {
+//         const response = await promise;// Wit until promise is resolved
+//         return response;
+//     } else {
+//         await Promise.reject(new Error('Intervening against document.write'))
+//     }
+//
+//
+// }
+//
+// myFunc()
+// .then(response => document.write(response))
+//     .catch(error => console.log(error));
+
