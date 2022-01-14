136f39c45ebb4efcf63766be4e19a8fa1326248a
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-14 13:42:53 -0600

    feat: created a POST using post() & promis()

--- js/easyhttp2.js
+++ js/easyhttp2.js
@@ -13,4 +13,24 @@ class EasyHTTP {
 
 
    }
+
+   //Make HTTP POST request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+
+
+    }
+
+
 }
--- js/http2.js
+++ js/http2.js
@@ -2,8 +2,20 @@
 
 const http = new EasyHTTP();
 
-//GET users
+// GET users
 http.get('https://jsonplaceholder.typicode.com/users')
     .then(data => console.log(data))
     .catch(error => console.log(error));
 
+
+//User Data
+const data = {
+    name: 'Jason Mccarthy',
+    username: 'jmac',
+    email: 'jmc@gmail.com'
+}
+
+//Create POST
+http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
