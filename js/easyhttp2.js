2c2445bdd900e33cecf64a2f50012c4816638e00
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-14 15:07:52 -0600

    feat: refactored http2 using async/await

--- js/easyhttp2.js
+++ js/easyhttp2.js
@@ -3,52 +3,99 @@
 class EasyHTTP {
 
     //Make HTTP GET request
-   get(url) {
-       return new Promise((resolve, reject) => {
-           fetch(url)
-               .then(result => result.json())
-               .then(data => resolve(data))
-               .catch(error => reject(error));
-       });
+   // get(url) {
+   //     return new Promise((resolve, reject) => {
+   //         fetch(url)
+   //             .then(result => result.json())
+   //             .then(data => resolve(data))
+   //             .catch(error => reject(error));
+   //     });
+   //
+   //
+   // }
+
+    //Using async/await
+    async get(url) {
+        const response = await fetch(url);
+
+        const responseData = await response.json();
+        return responseData;
+    }
 
 
-   }
+
 
    //Make HTTP POST request
-    post(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
+   //  post(url, data) {
+   //      return new Promise((resolve, reject) => {
+   //          fetch(url, {
+   //              method: 'POST',
+   //              headers : {
+   //                  'Content-type' : 'application/json'
+   //              },
+   //              body: JSON.stringify(data)
+   //          })
+   //              .then(result => result.json())
+   //              .then(data => resolve(data))
+   //              .catch(error => reject(error));
+   //      });
+   //
+   //
+   //  }
+
+    //Using async/await
+   async post(url, data) {
+
+         const response = await fetch(url, {
                 method: 'POST',
                 headers : {
                     'Content-type' : 'application/json'
                 },
                 body: JSON.stringify(data)
-            })
-                .then(result => result.json())
-                .then(data => resolve(data))
-                .catch(error => reject(error));
-        });
+            });
 
-
+          const responseData = await response.json();
+          return responseData;
     }
 
+
+
+
+
     //Make an HTTP PUT Request
-    put(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
+    // put(url, data) {
+    //     return new Promise((resolve, reject) => {
+    //         fetch(url, {
+    //             method: 'PUT',
+    //             headers : {
+    //                 'Content-type' : 'application/json'
+    //             },
+    //             body: JSON.stringify(data)
+    //         })
+    //             .then(result => result.json())
+    //             .then(data => resolve(data))
+    //             .catch(error => reject(error));
+    //     });
+    //
+    //
+    // }
+    //Using async/await
+
+   async put(url, data) {
+
+      const response = await fetch(url, {
                 method: 'PUT',
                 headers : {
                     'Content-type' : 'application/json'
                 },
                 body: JSON.stringify(data)
-            })
-                .then(result => result.json())
-                .then(data => resolve(data))
-                .catch(error => reject(error));
-        });
-
-
+            });
+      const responseData = await response.json();
+      return responseData;
     }
+
+
+
 
     //Make an HTTP DELETE Request
     delete(url) {
