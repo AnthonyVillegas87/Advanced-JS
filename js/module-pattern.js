b3b1c3d30a0072ba4608c468f40062fe260c404a
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-17 19:15:38 -0600

    feat: overview of mediator pattern

--- js/module-pattern.js
+++ js/module-pattern.js
@@ -133,61 +133,115 @@
 //         });
 
 
-        // //OBSERVER PATTERN
-        // function EventObserver() {
-        //     this.observers = [];
-        // }
-        //
-        // EventObserver.prototype = {
-        //     subscribe: function(fn) {
-        //         this.observers.push(fn);
-        //         console.log(`You are now subscribed to ${fn.name}!`);
-        //     },
-        //     unsubscribe: function(fn) {
-        //         //filter out from the list whatever matches the callback function. If there is no match,
-        //         //the callback stays on the list.The filter returns a new list & reassigns the list of observers
-        //
-        //         this.observers = this.observers.filter(function(item) {
-        //             if(item !== fn) {
-        //                 return item;
-        //             }
-        //         });
-        //         console.log(`You are now unsubscribed from ${fn.name}`);
-        //     },
-        //     fire: function() {
-        //         this.observers.forEach(function(item) {
-        //             item.call();
-        //         })
-        //     }
-        //
-        // }
-        //
-        // const click = new EventObserver();
-        //  //EVENT LISTENERS MILLISECONDS
-        // document.querySelector('.sub-ms').addEventListener('click', function() {
-        //     click.subscribe(getCurrMilliseconds)
-        // });
-        // document.querySelector('.unsub-ms').addEventListener('click', function() {
-        //     click.unsubscribe(getCurrMilliseconds)
-        // });
-        // document.querySelector('.fire').addEventListener('click', function() {
-        //     click.fire();
-        // });
-        //
-        // //SECONDS
-        // document.querySelector('.sub-s').addEventListener('click', function() {
-        //     click.subscribe(getCurrSeconds)
-        // });
-        // document.querySelector('.unsub-s').addEventListener('click', function() {
-        //     click.unsubscribe(getCurrSeconds)
-        // });
-        //
-        //
-        //
-        // //Click Handler
-        // const getCurrMilliseconds = function() {
-        //     console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`)
-        // }
-        // const getCurrSeconds = function() {
-        //     console.log(`Current Seconds: ${new Date().getSeconds()}`)
-        // }
+        //OBSERVER PATTERN
+    // function EventObserver() {
+    //     this.observers = [];
+    // }
+    //
+    // EventObserver.prototype = {
+    //     subscribe: function(fn) {
+    //         this.observers.push(fn);
+    //         console.log(`You are now subscribed to ${fn.name}!`);
+    //     },
+    //     unsubscribe: function(fn) {
+    //         //filter out from the list whatever matches the callback function. If there is no match,
+    //         //the callback stays on the list.The filter returns a new list & reassigns the list of observers
+    //
+    //         this.observers = this.observers.filter(function(item) {
+    //             if(item !== fn) {
+    //                 return item;
+    //             }
+    //         });
+    //         console.log(`You are now unsubscribed from ${fn.name}`);
+    //     },
+    //     fire: function() {
+    //         this.observers.forEach(function(item) {
+    //             item.call();
+    //         })
+    //     }
+    //
+    // }
+    //
+    // const click = new EventObserver();
+    //  //EVENT LISTENERS MILLISECONDS
+    // document.querySelector('.sub-ms').addEventListener('click', function() {
+    //     click.subscribe(getCurrMilliseconds)
+    // });
+    // document.querySelector('.unsub-ms').addEventListener('click', function() {
+    //     click.unsubscribe(getCurrMilliseconds)
+    // });
+    // document.querySelector('.fire').addEventListener('click', function() {
+    //     click.fire();
+    // });
+    //
+    // //SECONDS
+    // document.querySelector('.sub-s').addEventListener('click', function() {
+    //     click.subscribe(getCurrSeconds)
+    // });
+    // document.querySelector('.unsub-s').addEventListener('click', function() {
+    //     click.unsubscribe(getCurrSeconds)
+    // });
+    //
+    //
+    //
+    // //Click Handler
+    // const getCurrMilliseconds = function() {
+    //     console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`)
+    // }
+    // const getCurrSeconds = function() {
+    //     console.log(`Current Seconds: ${new Date().getSeconds()}`)
+    // }
+
+
+//Mediator Pattern
+const User = function(name) {
+    this.name = name;
+    this.chatroom = null;
+}
+
+User.prototype = {
+    send: function(message, to) {
+       this.chatroom.send(message, this, to)
+    },
+    receive: function(message, from) {
+        console.log(`${from.name} to ${this.name}: ${message}`)
+    }
+}
+
+const Chatroom = function() {
+    let users = {}; //list of users
+    return {
+        register: function(user) {
+            users[user.name] = user;
+            user.chatroom = this;
+        },
+        send: function(message, from, to) {
+            if(to) {
+                //SINGLE USER MESSAGE
+                to.receive(message, from);
+            } else {
+                //MASS MESSAGE
+                for(key in users) {
+                    if(users[key] !== from) {
+                        users[key].receive(message, from);
+                    }
+                }
+            }
+        }
+    }
+}
+const joe = new User('Joe');
+const george = new User('George');
+const matthew = new User('Matthew');
+const oj = new User('OJ');
+
+const chatroom = new Chatroom();
+
+chatroom.register(joe);
+chatroom.register(george);
+chatroom.register(matthew);
+chatroom.register(oj);
+
+joe.send('Hello george', george);
+oj.send('Hey Matt, can you tutor me this afternoon?', matthew);
+matthew.send('Anyone else need tutoring this afternoon?');
