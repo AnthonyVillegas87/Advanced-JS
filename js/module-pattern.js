3bc021bf8581ea4cb22df5ce5d4017732b412d73
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-17 18:16:26 -0600

    feat: Factory pattern overview

--- js/module-pattern.js
+++ js/module-pattern.js
@@ -10,7 +10,6 @@
 //
 // })();
 
-"use strict"
 
 //STANDARD MODULE PATTERN
 //         const UICtrl = (function() {
@@ -59,24 +58,76 @@
 //         );
 
 //SINGLETON PATTERN
+//         const Singleton = (function() {
+//            let instance;
+//
+//            function createInstance() {
+//                const object = new Object('Object Instance!');
+//                return object;
+//            }
+//
+//            return {
+//                getInstance: function() {
+//                    if(!instance) {
+//                        instance = createInstance();
+//                    }
+//                    return instance;
+//                }
+//            }
+//         })();
+//
+//         const instanceA = Singleton.getInstance();
+//         console.log(instanceA);
 
-const Singleton = (function() {
-   let instance;
+//FACTORY PATTERNS
+function MemberFactory() {
+    this.createMember = function(name, type) {
+        let member;
 
-   function createInstance() {
-       const object = new Object('Object Instance!');
-       return object;
-   }
+        if(type === 'basic') {
+            member = new BasicMembership(name);
+        } else if(type === 'standard') {
+            member = new StandardMembership(name);
+        } else if(type === 'premium') {
+            member = new PremiumMembership(name);
+        }
+        member.type = type;
 
-   return {
-       getInstance: function() {
-           if(!instance) {
-               instance = createInstance();
-           }
-           return instance;
-       }
-   }
-})();
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`)
+        }
+        return member;
+    }
+}
 
-const instanceA = Singleton.getInstance();
-console.log(instanceA);
+const BasicMembership = function(name) {
+    this.name = name;
+    this.cost = '$5';
+}
+const StandardMembership = function(name) {
+    this.name = name;
+    this.cost = '$10';
+}
+const PremiumMembership = function(name) {
+    this.name = name;
+    this.cost = '$25';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Smith', 'basic'));
+members.push(factory.createMember('Jake Ladd', 'standard'));
+members.push(factory.createMember('George Tudor', 'basic'));
+members.push(factory.createMember('Matthew Baker', 'premium'));
+members.push(factory.createMember('OJ Hill', 'standard'));
+members.push(factory.createMember('Shelby Davis', 'premium'));
+
+
+
+
+ console.log(members);
+
+members.forEach(function(member) {
+   member.define();
+})
